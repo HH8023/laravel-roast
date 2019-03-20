c00ad683d0e26dbe058e272478f1bd3301d5ad87
wangpeng <wangpeng@sinovine.com>
2019-03-20 17:19:29 +0800

    ceshi

--- app/Http/Controllers/Web/AuthenticationController.php
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -57,4 +57,46 @@ class AuthenticationController extends Controller
         return redirect('/');
     }
 
+    //测试
+    public function test()
+    {
+        $order = [
+            [
+                "order_count" => 17,
+                "uid" => 8505,
+                "order_price_sum" => 5398.6,
+                "nickname" => "张海鹏",
+            ],
+            [
+                "order_count" => 30,
+                "uid" => 8506,
+                "order_price_sum" => 8673.21,
+                "nickname" => "myw",
+            ],
+            [
+                "order_count" => 22,
+                "uid" => 8507,
+                "order_price_sum" => 2501.21,
+                "nickname" => "我",
+            ],
+        ];
+        $sale = [
+            [
+                "sale_count" => 4,
+                "uid" => 8506,
+                "sale_price_sum" => "769.20",
+                "sale_number_sum" => "7",
+                "nickname" => "myw",
+                "total" => 5384.4,
+            ],
+            [
+                "sale_count" => 2,
+                "uid" => 8507,
+                "sale_price_sum" => "96.80",
+                "sale_number_sum" => "3",
+                "nickname" => "我",
+                "total" => 290.4,
+            ],
+        ];
+    }
 }
